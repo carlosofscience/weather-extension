dc5efcc8b663de0d87a5540bc7f27c91c48b26b4
carlosofscience <carlosofscience@gmail.com>
2023-03-25 20:58:16 -0400

    building weather API interface

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useEffect }from 'react'
 import ReactDOM from 'react-dom/client'
 import './popup.css'
+import { fetchWeatherData } from '../utils/API'
+
 
 const rootElem = document.createElement('div')
 rootElem.id = "root"
 document.body.appendChild(rootElem)
-
-
 const root = ReactDOM.createRoot(rootElem);
+
+function App(){
+
+  useEffect(()=>{
+    fetchWeatherData("03053")
+    .then( data =>{
+      console.log(data.location.name);
+      console.log(data.current.temp_f);
+    })
+    .catch(err =>{
+      console.log(err);
+    })
+  }, [])
+
+   return (<>
+    <img src="icon.png" alt="Weather icon" />
+  </>)
+}
+
+
 
 root.render(
   <React.StrictMode>
-    <img src="icon.png" alt="Weather icon" />
-    <p>API key: { process.env.REACT_APP_WEATHER_API_KEY } </p>
+   <App />
   </React.StrictMode>
 );
--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,6 +1,37 @@
 const REACT_APP_WEATHER_API_KEY = '0f52057383af4a36b7b182759232503'
 
-export async function fetchOpenWeatherData(city: string): Promise<any>{
+export interface WeatherData{
+  location:{
+    name: string,
+    region: string,
+    country: string,
+    localtime: string,
+  },
+  current:{
+    temp_c: Number,
+    temp_f: Number,
+    is_day: Number,
+    condition: {
+      text: string,
+      icon: string,
+      code: Number
+    },
+    wind_mph: Number,
+    wind_kph: Number,
+    wind_degree: Number,
+    wind_dir: string,
+    pressure_mb: Number,
+    pressure_in: Number,
+    precip_mm: Number,
+    precip_in: Number,
+    humidity: Number,
+    cloud: Number,
+    feelslike_c: Number,
+    feelslike_f: Number,
+  }
+}
+
+export async function fetchWeatherData(city: string): Promise<WeatherData>{
   const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${REACT_APP_WEATHER_API_KEY}&q=${city}&aqi=yes`)
   
   if(!res.ok){
