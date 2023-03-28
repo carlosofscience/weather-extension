6fe99d805a1cdd8a02c4fc25e30203d7d6551447
carlosofscience <carlosofscience@gmail.com>
2023-03-27 22:09:33 -0400

    adding search input and results with material-ui

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,24 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import '@fontsource/roboto'
 import './popup.css'
 import WeatherCard from './WeatherCard'
 
+import { Box, InputBase, Grid, IconButton, Paper } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add';
+
 const rootElem = document.createElement('div')
 rootElem.id = "root"
 document.body.appendChild(rootElem)
 const root = ReactDOM.createRoot(rootElem);
-
+ 
 function App(){
 
+  const [zipcodes, setZipcodes] = useState<string[]>([
+    '03053',
+    '20171',
+    '10001',
+  ])
+
    return (<>
-      <WeatherCard zipcode='03053'/>
-      <WeatherCard zipcode='10001'/>
-      <WeatherCard zipcode='51'/>
+   <Box mx={'8px'} my={'16px'}>
+      <Grid>
+        <Grid>
+          <Paper>
+            <Box px={'15px'} py={'5px'}>
+              <InputBase placeholder='Add a zipcode or city'/>
+              <IconButton>
+                <AddIcon/>
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+      { zipcodes.map( (zipcode, index )=> <WeatherCard zipcode={zipcode} key={index}></WeatherCard>) }
+   </Box>
   </>)
 }
-
-
 
 root.render(
   <React.StrictMode>
