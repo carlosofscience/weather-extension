954379e46b109d19394febb3ea65897092adf885
carlosofscience <carlosofscience@gmail.com>
2023-03-27 22:40:44 -0400

    delete button for searched cities/zipcodes

--- src/popup/WeatherCard/WeatherCard.tsx
+++ src/popup/WeatherCard/WeatherCard.tsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState }from 'react'
 import { fetchWeatherData, WeatherData} from '../../utils/API'
-import { Card, CardContent, Typography, Box } from '@mui/material'
+import { Card, CardContent, Typography, Box, CardActions, Button} from '@mui/material'
 
 type weatherCardState = "loading" | "error" |  "ready"
 
 
 const WeatherCardContainer: React.FC<{
   children: React.ReactNode
-}> = ({children})=>{
+  onDelete?:()=> void
+}> = ({children, onDelete})=>{
   return   <Box mx={'4px'} my={'16px'}>
       <Card>
         <CardContent>{children}</CardContent>
+        <CardActions>
+          { onDelete && <Button color='secondary' onClick={onDelete}>Delete</Button> }
+        </CardActions>
       </Card>
     </Box>
 }
 
 const WeatherCard: React.FC<{
   zipcode: string
-}> = ({zipcode})=>{
+  onDelete?: ()=>void
+}> = ({zipcode, onDelete})=>{
 
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
   const [weatherCardState, setweatherCardState] = useState<weatherCardState>("loading")
@@ -34,7 +39,7 @@ const WeatherCard: React.FC<{
   }, [{zipcode}])
 
   if(weatherCardState == "loading" || weatherCardState == "error"){
-    return <WeatherCardContainer>
+    return <WeatherCardContainer onDelete={onDelete}>
       <Typography variant='body1'>
         {
           weatherCardState == "loading" ? "Loading..." : "Error: Could not retrieve weather data for this zipcode: " + zipcode
@@ -46,7 +51,7 @@ const WeatherCard: React.FC<{
 
 
   return (
-      <WeatherCardContainer>
+      <WeatherCardContainer onDelete={onDelete}>
         <Typography variant='h6'>
           {`${weatherData.location.name}, 
           ${weatherData.location.region}`}
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -6,6 +6,7 @@ import WeatherCard from './WeatherCard'
 
 import { Box, InputBase, Grid, IconButton, Paper } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
+import { Cipher } from 'crypto'
 
 const rootElem = document.createElement('div')
 rootElem.id = "root"
@@ -20,21 +21,39 @@ function App(){
     '10001',
   ])
 
-   return (<>
+  const [zipcodeInput, setZipcodeInput] = useState<string>("")
+
+    const handleZipcodeButtonOnClick = ()=>{
+
+      if (zipcodeInput === ''){
+        return
+      }
+      setZipcodes([...zipcodes, zipcodeInput])
+      setZipcodeInput('')
+      console.log(zipcodes);
+    }
+    
+    const handleZipcodeDeleteButtonOnClick= (index: number)=>{
+      zipcodes.splice(index, 1)
+      setZipcodes([...zipcodes])
+    }
+    
+    return (<>
    <Box mx={'8px'} my={'16px'}>
       <Grid>
         <Grid>
           <Paper>
             <Box px={'15px'} py={'5px'}>
-              <InputBase placeholder='Add a zipcode or city'/>
-              <IconButton>
+              <InputBase placeholder='Add a zipcode or city' value={zipcodeInput} onChange={(e)=> setZipcodeInput(e.target.value)}/>
+              <IconButton onClick={handleZipcodeButtonOnClick}>
                 <AddIcon/>
               </IconButton>
             </Box>
           </Paper>
         </Grid>
       </Grid>
-      { zipcodes.map( (zipcode, index )=> <WeatherCard zipcode={zipcode} key={index}></WeatherCard>) }
+      { zipcodes.map( (zipcode, index )=> <WeatherCard zipcode={zipcode} key={index} onDelete={()=>{handleZipcodeDeleteButtonOnClick(index)}}></WeatherCard>) }
+      <Box height={"16px"}></Box>
    </Box>
   </>)
 }
