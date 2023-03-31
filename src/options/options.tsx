8e24e6ff8296d2b972848c77f867d1fc03fb9539
carlosofscience <carlosofscience@gmail.com>
2023-03-30 20:16:54 -0400

    creating options page and homecity fields

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
+import '@fontsource/roboto'
 import './options.css'
+//importing material UI components
+import { Card, CardContent, Typography, TextField, Grid, Box, Button } from '@mui/material'
+
+import { getStoredOptions, setStoredOptions, LocalStorageOptions } from '../utils/storage'
 
 const App: React.FC<{}> = () => {
+
+  const [options, setOptions] = useState<LocalStorageOptions | null>(null)
+
+  useEffect(() => {
+    getStoredOptions().then(options => setOptions(options))
+  }, [])
+  
+  const handleHomeCityOnChange = (homeCity) =>{
+    setOptions({...options, homeCity})
+  }
+
+  if(!options){
+    return null
+  }
+
   return (
-    <div>
-      <img src="icon.png" />
-    </div>
+    <Box mx={'10%'} my={'2%'}>
+      <Card>
+      <CardContent>
+        <Grid container flexDirection={'column'} spacing={4}>
+          <Grid item>
+            <Typography variant='h4'>Weather Extension Options</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant='body1'>Home City name</Typography>
+            <TextField fullWidth placeholder='Enter a home city name' value={options.homeCity} onChange={e => handleHomeCityOnChange(e.target.value)}></TextField>
+          </Grid>
+          <Grid item>
+            <Button variant='contained' color='primary'>Save</Button>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+    </Box>
   )
 }
 
