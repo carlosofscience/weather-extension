378283b7de79e9f57caa94b86500486e613c28c6
carlosofscience <carlosofscience@gmail.com>
2023-03-31 13:45:06 -0400

    implementing context menu feature

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,4 @@
-import { setStoredCities, setStoredOptions } from "../utils/storage";
+import { getStoredCities, setStoredCities, setStoredOptions } from "../utils/storage";
 
 // TODO: background script
 chrome.runtime.onInstalled.addListener(() => {
@@ -8,5 +8,17 @@ chrome.runtime.onInstalled.addListener(() => {
     tempScale: 'metric',
     homeCity:'',
     hasAutoOverlay: false
+  })
+
+  chrome.contextMenus.create({
+    contexts: ['selection'],
+    title: "Add city to weather extension",
+    id: 'weatherExtension'
+  })
+})
+//outside of onIntalled since context menu listeners should be install after service woker wakes up 
+chrome.contextMenus.onClicked.addListener( e =>{
+  getStoredCities().then( cities =>{
+    setStoredCities([...cities, e.selectionText])
   })
 })
